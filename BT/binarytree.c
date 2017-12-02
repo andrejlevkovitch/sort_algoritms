a16946285899203a80e277bd0ed3ff434edf67fc
andrejlevkovitch <andrejlevkivitch@gmail.com>
2017-12-02 22:14:16 +0300

    добавлены операции нахождение минимума и максимума в дереве поиска и флаги

--- BT/binarytree.c
+++ BT/binarytree.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define SIZE 10
+#define nFlags 1
 
 struct node {//структура узла
     int info;//элемент, содержащийся в узле
@@ -23,41 +24,101 @@ bool insert (struct tree *, int);//добавление элемента
 bool rm (struct tree *, int);//удаление элемента
 void walk (const struct node *);//хотьба по дереву и печать элементов начиная с входного узла
 void print_tree (const struct tree *);//печать структуры
+struct node *min (const struct tree *);
+struct node *max (const struct tree *);
 
-int main (void)
+int main (int argc, char *argv [])
 {
-    struct tree *arbo = create ();
-    struct node **out_node = &arbo->root;//ВАЖНО!!! это указатель на указатель корня! Иначе нода просто останется равной NULL
+    int kurrentFlag = 0;
+    char *validFlags [nFlags] = {"-help"};
+    for (kurrentFlag = 0; kurrentFlag < nFlags; ++kurrentFlag) {
+        if (!strcmp (argv [1], validFlags [kurrentFlag]))
+            break;
+    }
+    if (argc > 1) {
+        switch (kurrentFlag) {
+            case 0:
+                printf ("This program performs the following operations:\n");
+                printf ("+ adding an element;\n");
+                printf ("- removal of an element;\n");
+                printf ("s element search;\n");
+                printf ("a output of the minimal element;\n");
+                printf ("z output of the maximal element;\n");
+                printf ("p output of the tree.\n");
+                printf ("\nIf you want to exit input cntrl+d\n");
+                break;
+            default:
+                printf ("Flag does not exists.\n");
+                break;
+        }
 
-    int array [SIZE] = {5, 0, 1, 3, 2, 4, 6, 8, 7, 9};
-
-    for (unsigned int i = 0; i < SIZE; ++i) {
-        if (search (arbo, array [i]))
-            printf ("this element (%i) already exists in the tree\n", array [i]);
-        else
-            if (insert (arbo, array [i]))
-                printf ("this element (%i) is successfully pushed into the tree\n", array [i]);
-            else
-                printf ("this element (%i) not was pushed into the tree\n", array [i]);
+        return EXIT_SUCCESS;
     }
 
-    printf ("%i\n", arbo->size);
-    if (*out_node)
-        printf ("root == %i\n", (*out_node)->info);
+    struct tree *arbo = create ();
+    struct node *exm = NULL;
 
-    walk (arbo->root);
-    printf ("\n");
+    int tmp = 0;
+    char operace = 0;
 
-    if (rm (arbo, 5))
-        printf ("element %i is delete saccessfully\n", 5);
-    else
-        printf ("element %i not was delete\n", 5);
+    printf ("This program creates a binary tree of search\n");
 
-    if (*out_node)
-        printf ("root == %i\n", (*out_node)->info);
+    do {
+        printf ("Please, input operace and element.\n");
+        switch (operace = getchar ()) {
+            case '+':
+                if (scanf ("%i", &tmp)) {
+                    if (insert (arbo, tmp))
+                        printf ("Element %i is successfully pushed into the tree.\n", tmp);
+                    else
+                        printf ("Element %i was not inserted into the tree.\n", tmp);
+                }
+                break;
+            case '-':
+                if (scanf ("%i", &tmp)) {
+                    if (rm (arbo, tmp))
+                        printf ("Element %i was removed from the tree.\n", tmp);
+                    else
+                        printf ("Element %i was not removed from the tree.\n", tmp);
+                }
+                break;
+            case 's':
+                if (scanf ("%i", &tmp)) {
+                    if (search (arbo, tmp))
+                        printf ("Element %i already exists in the tree.\n", tmp);
+                    else
+                        printf ("Element %i was not searched in the tree.\n", tmp);
+                }
+                break;
+            case 'p':
+                printf ("The tree have %i elements:\n", arbo->size);
+                print_tree (arbo);
+                printf ("\n");
+                break;
+            case EOF:
+                break;
+            case 'a':
+                exm = min (arbo);
+                if (exm)
+                    printf ("MinElement in the tree == %i\n", exm->info);
+                else
+                    printf ("MinElement was not searched in the tree.\n");
+                break;
+            case 'z':
+                exm = max (arbo);
+                if (exm)
+                    printf ("MaxElement in the tree == %i\n", exm->info);
+                else
+                    printf ("MaxElement was not searched in the tree.\n");
+                break;
+            default:
+                printf ("Unknow operace!\n");
+                break;
+        }
 
-    print_tree (arbo);
-    printf ("\n");
+        while (operace != '\n' && operace != EOF && getchar () != '\n')
+            continue;
+    } while (operace != EOF);
 
     return EXIT_SUCCESS;
 }
@@ -187,9 +248,9 @@ void walk (const struct node *kur_node)
     if (!kur_node)
         return;
     else {
-        walk (kur_node->left);
+        walk (kur_node->left);//рекурсия влево - пока не упрется в самый маленький элемент
         printf ("%i ", kur_node->info);
-        walk (kur_node->right);
+        walk (kur_node->right);//рекурсия вправо
     }
 
     return;
@@ -198,4 +259,31 @@ void walk (const struct node *kur_node)
 void print_tree (const struct tree *kur_tree)
 {
     walk (kur_tree->root);
+}
+
+struct node *min (const struct tree *search_tree)
+{
+    struct node *search_node = search_tree->root;
+
+    if (!search_node)
+        return NULL;
+
+    while (search_node->left) {
+        search_node = search_node->left;
+    }
+
+    return search_node;
+}
+
+struct node *max (const struct tree *search_tree)
+{
+    struct node *search_node = search_tree->root;
+
+    if (!search_node)
+        return NULL;
+    while (search_node->right) {
+        search_node = search_node->right;
+    }
+
+    return search_node;
 }
